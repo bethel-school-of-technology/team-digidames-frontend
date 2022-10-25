4f638c7e821088ca617b801943185bcced8f133d
Hutchins <lori.hutchins@indwes.edu>
2022-10-24 22:12:51 -0400

    updated  with context hooks

--- summareasy-react-app/src/App.js
+++ summareasy-react-app/src/App.js
@@ -5,7 +5,7 @@ import API from './UTILS/API';
 import OneBookReport from './Components/OneBookReport';
 import { Routes, Route, useNavigate } from "react-router-dom";
 import UpdateBookReport from "./Components/UpdateBookReport";
-
+import BookReportContext from './UTILS/BookReportContext';
 
 function App() {
   const [allBookReports, setAllBookReports] = useState([]);
@@ -16,120 +16,139 @@ function App() {
   });
 
   const [refresh, setRefresh] = useState({ count: 0 });
-
   const navigate = useNavigate();
 
   useEffect(() => {
-
     getAllBookReports();
-
-  }, [])
-
+  }, []);
 
   const getAllBookReports = () => {
     API.getAll().then(res => {
-
-
       setAllBookReports(res.data)
-    })
-  };
-
-  const handleTitleChange = (e) => {
-
-    const { value } = e.target;
-
-    setNewBookReport({ ...newBookReport, title: value })
-
-  };
-
-  const handleAuthorChange = (e) => {
-
-    const { value } = e.target;
-
-    setNewBookReport({ ...newBookReport, author: value })
-
-  };
-
-  const handleReportChange = (e) => {
-
-    const { value } = e.target;
-
-    setNewBookReport({ ...newBookReport, report: value })
-
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    API.createBookReport(newBookReport).then(res => {
-      console.log(res);
     });
-  }
+  };
+
+  //Will Remove once I know the it works with out
+  // const handleTitleChange = (e) => {
+  //   const { value } = e.target;
+  //   setNewBookReport({ ...newBookReport, title: value });
+  // };
+
+  // const handleAuthorChange = (e) => {
+  //   const { value } = e.target;
+  //   setNewBookReport({ ...newBookReport, author: value })
+  // };
+
+  // const handleReportChange = (e) => {
+  //   const { value } = e.target;
+  //   setNewBookReport({ ...newBookReport, report: value })
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   API.createBookReport(newBookReport).then(res => {
+  //     console.log(res);
+  //   });
+  // }
+
+
+
+  // const updateBookReport = (id) => {
+
+  //   API.updateBookReport(id, newBookReport).then(res => {
+  //     console.log(res);
+  //     setRefresh({ ...refresh, count: refresh.count + 1 });
+  //     navigate("/") //this navigates (useNavigat) to homepage ("/") after updating Book Report
+  //   });
+  // }
+
+  // const handleDelete = (id) => {
+  //   API.deleteBookReport(id).then(res => {
+  //     console.log(res);
+  //     setRefresh({ ...refresh, count: refresh.count + 1 });
+  //     navigate("/") //this navigates (useNavigat) to homepage ("/") after deleting Book Report      
+  //   });
+  // };
 
   useEffect(() => {
     getAllBookReports();
   }, [refresh]);
 
-  const updateBookReport = (id) => {
 
-    API.updateBookReport(id, newBookReport).then(res => {
-      console.log(res);
-      setRefresh({ ...refresh, count: refresh.count + 1 });
-      navigate("/") //this navigates (useNavigat) to homepage ("/") after updating Book Report
-    });
+  const contextObject = {
+    handleSubmit: (e) => {
+      e.preventDefault();
+      API.createBookReport(newBookReport).then((res) => {
+        console.log(res);
+        setRefresh({ ...refresh, count: refresh.count + 1 });
+        document.querySelector(".forms").reset();
+      });
+    },
 
-  }
+    handleTitleChange: (e) => {
+      const { value } = e.target;
+      setNewBookReport({ ...newBookReport, title: value });
+    },
 
-  const handleDelete = (id) => {
-    API.deleteBookReport(id).then(res => {
-      console.log(res);
-      setRefresh({ ...refresh, count: refresh.count + 1 });
-      navigate("/") //this navigates (useNavigat) to homepage ("/") after deleting Book Report      
-    })
+    handleAuthorChange: (e) => {
+      const { value } = e.target;
+      setNewBookReport({ ...newBookReport, author: value });
+    },
+
+    handleReportChange: (e) => {
+      const { value } = e.target;
+      setNewBookReport({ ...newBookReport, report: value });
+    },
+
+    updateBookReport: (id) => {
+      API.updateBookReport(id, newBookReport).then(res => {
+        console.log(res);
+        setRefresh({ ...refresh, count: refresh.count + 1 });
+        navigate("/") //this navigates (useNavigat) to homepage ("/") after updating Book Report
+      });
+    },
+
+    handleDelete: (id) => {
+      API.deleteBookReport(id).then(res => {
+        console.log(res);
+        setRefresh({ ...refresh, count: refresh.count + 1 });
+        navigate("/") //this navigates (useNavigat) to homepage ("/") after deleting Book Report      
+      });
+    }
+
+
   }
 
   return (
     <div className="App">
-      <Routes>
-        <Route
-          path="/"
-          exact
-          element={
-            <AllBookReports
-              bookReportData={allBookReports}
-              handleTitleChange={handleTitleChange}
-              handleAuthorChange={handleAuthorChange}
-              handleReportChange={handleReportChange}
-              handleSubmit={handleSubmit}
-              handleDelete={handleDelete}
-            />
-          }
-        />
+      <BookReportContext.Provider value={contextObject}>
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <AllBookReports
+                bookReportData={allBookReports} />
+            }
+          />
 
-        <Route
-          path="/one-bookreport/:id"
-          element={
-            <OneBookReport
-              handleDelete={handleDelete}
-            />
-          }
-        />
+          <Route
+            path="/one-bookreport/:id"
+            element={
+              <OneBookReport />
+            }
+          />
 
-        <Route
-          path="/update-bookreport/:id"
-          element={
-            <UpdateBookReport
-              handleTitleChange={handleTitleChange}
-              handleAuthorChange={handleAuthorChange}
-              handleReportChange={handleReportChange}
-              updateBookReport={updateBookReport}
+          <Route
+            path="/update-bookreport/:id"
+            element={
+              <UpdateBookReport />
+            }
+          />
 
-            />
-          }
-        />
-
-      </Routes>
-
+        </Routes>
+      </BookReportContext.Provider>
     </div>
   );
 }
