586d38ca9127164b377abbca359fe58f071b4cdb
Hutchins <lori.hutchins@indwes.edu>
2022-10-29 16:26:50 -0400

    updated the Footer with Bootstrap, styled the a tag for About Us

--- summareasy-react-app/src/Components/Footer/index.js
+++ summareasy-react-app/src/Components/Footer/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import sun from "../../images/sun.jpg.png"
+import { Col, Container, Row } from 'react-bootstrap';
+import "../../App.css";
+
 function Footer() {
     let today = new Date();
     let day = today.getDate();
@@ -7,17 +10,39 @@ function Footer() {
     let year = today.getFullYear();
 
     return (
-        <footer>   
-            <p ><span>Summareasy &copy; 2022 </span>
-            <img
-                alt=""
-                src={sun}
-                width="50"
-                style={{ padding: "10px" }}
-            ></img>
-            <span>Today is { month }/{day}/{ year }</span></p> 
+        <footer >
+            <Row style={{fontFamily:'Amaranth', fontSize:'30px', padding: '19px', color: 'rgba(0,0,0)'}}>
+                <Col>
+                    <img
+                        alt=""
+                        src={sun}
+                        width="50"
+                        style={{ padding: "10px" }}
+                    ></img>
+                
+                    <span>Summareasy &copy; 2022 </span>
+                </Col>
+                <Col>
+                    <span>Today is {month}/{day}/{year}</span>
+                </Col>
+                <Col>
+                    <span><a href={`/about`} class='aboutUsLink'>About DigiDames</a></span>
+                </Col>
+            </Row>
         </footer>
     )
 }
 
 export default Footer;
+
+
+{/* <Row key={i} style={{paddingBottom: '100px', paddingRight: '100px'}}>
+<Col xs='12' sm='12' md='5' lg='5' xl='6'>
+    <img variant="top" src={image} style={{padding: '10px'}} width= '350px' alt='digidameteammate'/>
+    <h4 style={{fontSize: '28px', fontWeight: 'bold', fontFamily: 'Teko'}}>{name}</h4>
+</Col>
+<Col xs='12' sm='12' md='7' lg='7' xl='6'>
+    <p style={{fontSize: '28px', textAlign: 'center', fontWeight: 'bold', fontFamily: 'Teko'}}>{role}</p>
+    <p style={{fontSize: '22px', textAlign: 'center', fontFamily: 'Teko'}}>{about}</p>
+</Col>
+</Row> */}
